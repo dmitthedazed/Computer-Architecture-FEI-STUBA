166200d2fd41792cf6d0a72e5a01f57ff1937a50
Dmitrii Savin <89397034+dmitthedazed@users.noreply.github.com>
2025-06-21 22:36:06 +0200

    Create Uvod.c

---
+++ Uvod.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void mojaFunkcia()
+{
+    for (int i = 1; i <= 100; i++)
+        printf("%d\n", i);
+}
+
+void nemojaFunkcia()
+{
+    for (int a = 1; a <= 30; a++)
+        if (a % 2 == 0) {
+            printf("%d\n", a);
+        }
+}
+
+int main()
+{
+    printf("Nazdar!!!\n");
+    mojaFunkcia();
+    nemojaFunkcia();
+    return 0;
+}
